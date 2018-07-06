b12849c9b300fb5e0a45a4f82dea56fe9e2cc421
Rafał Rudol <rrrrudol@gmail.com>
2018-07-06 22:05:16 +0200

    fix #1 font loading problem

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,11 @@
-require([
-  "Game",
-  "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.1/pixi.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"
-], (Game, pixi, io) => {
-  new Game(pixi, io);
-});
+window.onload = function(){
+  require([
+    "Game",
+    "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.1/pixi.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"
+  ], (Game, pixi, io) => {
+    setTimeout(() => { new Game(pixi, io); }, 1000);
+  });  
+};
+
+
